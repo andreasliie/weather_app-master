ded19a91965a299922c6d58c4f95763c23afb108
Andreas Lie <andreas.gulbr@gmail.com>
2022-08-28 19:57:04 +0200

    main

--- lib/presentation/pages/location.dart
+++ lib/presentation/pages/location.dart
@@ -56,15 +56,30 @@ class _LocationsState extends State<Locations> {
             ),
             new Container(
               width: 300.0,
-              height: 100,
+              height: 55,
               alignment: Alignment.center,
-              child: Text("Check forcast",
+              child: Text("weather forcast",
                   style: TextStyle(
                       fontSize: 35,
                       height:
                           2, //line height 200%, 1= 100%, were 0.9 = 90% of actual line height
-                      color: Color.fromARGB(255, 189, 0, 214), //font color
-                      letterSpacing: 1, //letter spacing
+                      color: Color.fromARGB(255, 255, 255, 255), //font color
+                      letterSpacing: 2, //letter spacing
+                      decorationThickness:
+                          1.0, //decoration 'underline' thickness
+                      fontStyle: FontStyle.normal)),
+            ),
+            new Container(
+              //width: 200.0,
+              height: 40,
+              alignment: Alignment.center,
+              child: Text("by andreas lie gulbrandsen",
+                  style: TextStyle(
+                      fontSize: 10,
+                      height:
+                          2, //line height 200%, 1= 100%, were 0.9 = 90% of actual line height
+                      color: Color.fromARGB(255, 255, 255, 255), //font color
+                      letterSpacing: 2, //letter spacing
                       decorationThickness:
                           1.0, //decoration 'underline' thickness
                       fontStyle: FontStyle.normal)),
@@ -130,83 +145,5 @@ class _LocationsState extends State<Locations> {
         ),
       ]),
     );
-
-    // @override
-    // Widget build(BuildContext context) {
-    //   return Scaffold(
-    //     appBar: AppBar(
-    //       title: Text('Weather App'),
-    //     ),
-    //     body: SafeArea(
-    //       child: Column(
-    //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //         crossAxisAlignment: CrossAxisAlignment.center,
-    //         children: <Widget>[
-    //           Row(
-    //             mainAxisAlignment: MainAxisAlignment.center,
-    //             children: <Widget>[
-    //               TextButton(
-    //                   onPressed: () async {
-    //                     var weatherData = await weatherModel.getLocationWeather();
-    //                     updateUI(weatherData);
-    //                   },
-    //                   child: Icon(
-    //                     Icons.near_me,
-    //                     color: white,
-    //                     size: 50.0,
-    //                   )),
-    //             ],
-    //           ),
-    //           Container(
-    //             padding: EdgeInsets.all(5.0),
-    //             child: TextField(
-    //               style: TextStyle(color: black),
-    //               decoration: textFieldDecoration,
-    //               onChanged: (value) {
-    //                 cityName = value;
-    //               },
-    //             ),
-    //           ),
-    //           TextButton(
-    //             onPressed: () async {
-    //               if (cityName != null) {
-    //                 var weatherData = await weatherModel.getCityWeather(cityName);
-    //                 updateUI(weatherData);
-    //               }
-    //             },
-    //             child: Text(
-    //               'Get Weather',
-    //               style: kButtonTextStyle,
-    //             ),
-    //           ),
-    //           Padding(
-    //             padding: EdgeInsets.only(left: 15.0),
-    //             child: Row(
-    //               children: <Widget>[
-    //                 Text(
-    //                   '$temperature°  ',
-    //                   style: kTempTextStyle,
-    //                 ),
-    //                 Text(
-    //                   '$weatherIcon',
-    //                   style: kConditionTextStyle,
-    //                 ),
-    //               ],
-    //             ),
-    //           ),
-    //           Flexible(
-    //             child: Padding(
-    //               padding: EdgeInsets.only(right: 15.0),
-    //               child: Text(
-    //                 '$tempIcon in $cityName',
-    //                 //textAlign: TextAlign.center,
-    //                 style: kMessageTextStyle,
-    //               ),
-    //             ),
-    //           ),
-    //         ],
-    //       ),
-    //     ),
-    //   );
   }
 }
